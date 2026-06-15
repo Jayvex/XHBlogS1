480368ec26e9fb38769a86f3374c07be2ac8daa1
Jayvex <2023683967@qq.com>
2026-06-15 21:53:48 +0800

    一些信息的修改

--- data/projects.ts
+++ data/projects.ts
@@ -11,14 +11,13 @@ export type Project = {
 
 export const projectsData: Project[] = [
   {
-    "id": "proj_1775049332705",
-    "name": "Computational Chemistry Tool",
-    "githubUrl": "https://github.com/heiehiehi/Computational_Chemistry_Tool",
-    "description": "该工具本作者使用在Win下的WSL2平台，系统为Ubuntu22，个人使用请依据自己数据进行修改（这些工具只是整合了一些流程）",
+    "id": "proj_1781531355352",
+    "name": "中国象棋",
+    "githubUrl": "https://github.com/Jayvex/china_chess",
+    "description": "可通过访问https://xqj.jayvex.xyz/进行游玩",
     "icon": "🚀",
     "tags": [
-      "Gromacs",
-      "RMSF"
+      "react"
     ]
-  },
+  }
 ];
